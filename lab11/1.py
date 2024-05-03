92c8cbe1f4cd7ab1dd50cdc4fa932c6d41691fda
aizatkenges <157211797+aizatkenges@users.noreply.github.com>
2024-05-03 21:10:42 +0500

    lab11

---
+++ lab11/1.py
@@ -0,0 +1,25 @@
+import psycopg2
+def find(pattern):
+    config = psycopg2.connect(
+        host = 'localhost',
+        database='phonebook',
+        user='postgres',
+        password='zzzz06'
+    )
+
+    current = config.cursor()
+    sql = '''
+        SELECT * FROM phone_number WHERE name LIKE %s OR surname LIKE %s OR number_value LIKE %s
+
+    '''
+    pattern2=pattern+'%'
+    current.execute(sql, (pattern2,  pattern2, pattern2))
+    record = current.fetchall()#іздеген нәрсені тюпл ретінде қайтарады
+    if record:
+        for i in record:
+            print(i)
+    else:
+        print('Data not found! :(')
+
+pattern = input('What do you want to find?: ')
+find(pattern)
